a2f51035cbd984b6b5e3bade8d516f660e54d236
2006040123 <132646918+2006040123@users.noreply.github.com>
2023-05-05 18:47:09 +0800

    Update test_01.py

--- test/test_01.py
+++ test/test_01.py
@@ -1,9 +1,7 @@
 # ！不要修改这里的内容
-from _01_Hello_World.Hello_World import main
-
 
 def test():
-    assert main() == "Hello, World!"
+    assert "Hello,World" == "Hello, World!"
 
 
 if __name__ == '__main__':
